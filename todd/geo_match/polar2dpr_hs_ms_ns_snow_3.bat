6a408ab0e0a76ee4c7bfb6a2cab6b0af48267e09
tberende <tberende@MSSDR18080793.ndc.nasa.gov>
2019-12-04 13:40:51 -0500

    Test for non-pps filename convention GPM OP files for dual-radar

--- todd/geo_match/polar2dpr_hs_ms_ns_snow_3.bat
+++ todd/geo_match/polar2dpr_hs_ms_ns_snow_3.bat
@@ -31,7 +31,7 @@ FILES4NC = GETENV("CONTROLFILE")
 polar2dpr_hs_ms_ns_snow, FILES4NC, 100, SCORES=0, GPM_ROOT=gpm_root, $
            DIRGV='/data/gpmgv/gv_radar/finalQC_in', PLOT_PPIS=0, $
            NC_DIR='/data/gpmgv/netcdf/grmatch', DIR2ADPR='/.', DIR_BLOCK='/data/gpmgv/blockage', $
-           DPR_DBZ_MIN=15.0, DBZ_MIN=15.0, NC_NAME_ADD='15dbzGRDPR'
+           DPR_DBZ_MIN=15.0, DBZ_MIN=15.0, NC_NAME_ADD='15dbzGRDPR', non_pps_files=1
  
 ;resolve_all
 ;save, /routines, file='/home/tberendes/git/gpmgv/todd/geo_match/polar2dpr_hs_ms_ns_snow.sav'
