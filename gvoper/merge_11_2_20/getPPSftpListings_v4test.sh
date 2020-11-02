9619216645144c494195507da3a9c79243532287
tberendes <berendt@uah.edu>
2020-11-02 13:30:29 -0600

    Added holding directory for original verions of gvoper merged files

---
+++ gvoper/merge_11_2_20/getPPSftpListings_v4test.sh
@@ -0,0 +1,301 @@
+#!/bin/sh
+#
+# getPPSftpListings_v4test.sh    Morris/SAIC/GPM GV    April 2014
+#
+# DESCRIPTION:
+#
+# Uses Perl script 'mirror' to create a local mirror of the PPS ftp location
+# where ftp_url_YYMMDDHHMM.txt listing files for new Internal Test and
+# Evaluation (ITExxx, xxx=001,002,...) PPS products are located.  Makes up to 10
+# attempts to access the ftp site and download all the files not present in the
+# local directory, $MIR_DATA_DIR.  Examines MIR_LOG_FILE to determine whether
+# any errors occurred in the last run of mirror, and if so, makes a retry after
+# a sleep period.
+#
+# FILES:
+#
+#                  mirror - Perl script to perform the directory mirroring via
+#                           ftp.  Freeware download.
+#
+#         mirror.defaults - Default configuration items for mirror script.  Used
+#                           by mirror but not referenced in this script.  May be
+#                           augmented and/or overridden by package file.
+#
+# arthurhou.pps.eosdis.nasa.gov.v4test - Package file, holds configuration for
+#                           mirror script to mirror ITE files from PPS ftp site.
+#
+#        pps_ftp_lists.log - Log file of mirror script.  Is examined to determine
+#                           if download errors require mirror to be re-run.
+#                           Is renamed mirror.YYMMDD.log and moved from
+#                           $MIR_DATA_DIR to $LOG_DIR at end of script run.
+#
+#  DATABASE
+#    Catalogs PPS subset file metadata in orbit_subset_product table in 'gpmgv'
+#    database in PostGRESQL via call to psql utility.
+#
+#  LOGS
+#    Output for day's script run logged to daily log file
+#    getPPSftpListings.YYYYMMDD.log in data/logs subdirectory. 
+#    YYYYMMDD is replaced by the current date.
+#
+#  CONSTRAINTS
+#    - User under which script is run must have access privileges to PostGRESQL
+#      database 'gpmgv', and INSERT privileges on table.  Utility 'psql' must
+#      be in user's $PATH.
+#    - User must have write privileges in $MIR_DATA_DIR, $LOG_DIR directories
+#
+#  HISTORY
+#    04/14/2014, Morris, SAIC, GPM GV
+#    - Created from getPRdata.sh.
+#    08/26/2014      - Morris
+#    - Changed LOG_DIR to /data/logs and TMP_DIR to /data/tmp
+#    2015, Morris, SAIC, GPM GV
+#    - Created from getPPSftpListings.sh
+#
+################################################################################
+
+
+GV_BASE_DIR=/home/gvoper
+DATA_DIR=/data/emdata      # not used in script
+TMP_DIR=/data/tmp
+LOG_DIR=/data/logs
+BIN_DIR=${GV_BASE_DIR}/scripts
+
+# Following two variables must be the same as specified in the "package" file!
+MIR_DATA_DIR=${TMP_DIR}/PPS_v4test
+MIR_LOG_FILE=${MIR_DATA_DIR}/pps_ftp_lists.log
+
+MIR_BIN=mirror
+MIR_BIN_DIR=${BIN_DIR}/mirror
+# The following are specified relative to MIR_BIN_DIR, as mirror expects:
+MIR_PACKAGES_DIR=packages
+MIR_PACKAGE=${MIR_PACKAGES_DIR}/arthurhou.pps.eosdis.nasa.gov.v4test
+
+# Constants for possible status of downloads, for appstatus table in database
+UNTRIED='U'    # haven't attempted initial download yet
+SUCCESS='S'    # got the desired data files, if any
+MISSING='M'    # initial tries failed, try again next time
+FAILED='F'     # failed Nth round of tries, no more attempts
+INCOMPLETE='I' # could not complete all steps, could be external problem
+
+# re-used file to hold output from database queries
+DBTEMPFILE=${TMP_DIR}/getPPSftpListings_dbtempfile
+
+# ZZZ is number of seconds to sleep between repeat mirror attempts if problems/errors
+ZZZ=300
+rundate=`date -u +%y%m%d`
+LOG_FILE=${LOG_DIR}/getPPSftpListings_v4test.${rundate}.log
+export rundate
+
+umask 0002
+
+echo "Starting mirroring run for PPS FTP listings for $rundate." | tee -a $LOG_FILE
+echo "=======================================================" | tee -a $LOG_FILE
+echo "" | tee -a $LOG_FILE
+
+pgproccount=`ps -ef | grep postgres | grep -v grep | wc -l`
+
+if [ ${pgproccount} -lt 3 ]
+  then
+    HOST=`hostname`
+    thistime=`date -u`
+    echo "Message from getPPSftpListings.sh cron job on ${thistime}:" \
+      > /tmp/PG_MAIL_ERROR_MSG.txt
+    echo "" >> /tmp/PG_MAIL_ERROR_MSG.txt
+    echo "${pgproccount} Postgres processes active, should be 3 !!" \
+      >> /tmp/PG_MAIL_ERROR_MSG.txt
+    echo "NEED TO RESTART POSTGRESQL ON ${HOST}." >> /tmp/PG_MAIL_ERROR_MSG.txt
+    mailx -s 'postgresql down on ${HOST}' \
+      -c kenneth.r.morris@nasa.gov,todd.a.berendes@nasa.gov,berendt@uah.edu \
+      makofski@radar.gsfc.nasa.gov < /tmp/PG_MAIL_ERROR_MSG.txt
+    cat /tmp/PG_MAIL_ERROR_MSG.txt | tee -a $LOG_FILE
+    exit 1
+  else
+    echo "${pgproccount} Postgres processes active, should be 3." \
+      | tee -a $LOG_FILE
+    echo "" | tee -a $LOG_FILE
+fi
+#exit  #uncomment for just testing e-mail notifications
+
+# initialize the appstatus table entry for this run's yymmdd
+echo "Checking whether we have an entry for this yymmdd in database:"\
+ | tee -a $LOG_FILE
+echo "\pset format u \t \o ${DBTEMPFILE} \\\ SELECT status FROM appstatus \
+ WHERE app_id = 'getV4tesList' AND datestamp = '${rundate}';" \
+  | psql -a -d gpmgv  | tee -a $LOG_FILE 2>&1
+
+echo "" | tee -a $LOG_FILE
+if [ -s ${DBTEMPFILE} ]
+  then
+     # We've tried to do this yymmdd before, get our past status.
+     status=`cat ${DBTEMPFILE}`
+     echo "Rundate ${rundate} has been attempted before with status = $status."\
+     | tee -a $LOG_FILE
+     if [ $status = $SUCCESS ]
+       then
+         echo "PRIOR RUN SUCCESSFUL.  SCRIPT getPPSftpListings.sh COMPLETE, EXITING." \
+	       | tee -a $LOG_FILE
+         echo "See log file $LOG_FILE for information."
+         exit 0
+     fi
+  else
+     # Empty file indicates no row exists for this file datestamp, so insert one
+     # now with defaults for first_attempt and ntries columns
+     echo "" | tee -a $LOG_FILE
+     echo "INSERT INTO appstatus (app_id, datestamp, status) VALUES \
+       ('getV4tesList', '${rundate}', '$UNTRIED');" | psql -a -d gpmgv \
+       | tee -a $LOG_FILE 2>&1
+fi
+
+# increment the ntries column in the appstatus table for $rundate
+echo "" | tee -a $LOG_FILE
+echo "UPDATE appstatus SET ntries=ntries+1 WHERE app_id = 'getV4tesList' AND \
+ datestamp = '$rundate';" | psql -a -d gpmgv  | tee -a $LOG_FILE 2>&1
+echo "" | tee -a $LOG_FILE
+
+# mirror (apparently) must be run from its home directory
+cd ${MIR_BIN_DIR}
+
+if [ ! -x ${MIR_BIN} ]
+  then
+     echo "Executable file '${MIR_BIN}' not found in `pwd`, exiting." \
+          | tee -a $LOG_FILE
+     echo "UPDATE appstatus SET status = '$FAILED' \
+           WHERE app_id = 'getV4tesList' AND datestamp = '$rundate';" \
+           | psql -a -d gpmgv  | tee -a $LOG_FILE 2>&1
+     exit 1
+fi
+
+MIRCMD=./${MIR_BIN}' '${MIR_PACKAGE}
+runagain='y'
+timedout='n'
+declare -i tries=0
+
+until [ "$runagain" = 'n' ]
+  do
+     tries=tries+1
+     echo "Try = ${tries}, max = 10." | tee -a $LOG_FILE
+     echo "Running following command from `pwd` :"
+     echo ${MIRCMD}
+
+# ******* run mirror command here ********
+     ${MIRCMD} | tee -a $LOG_FILE
+
+# bailout mechanism for testing
+#     echo "Enter q to quit or hit return to continue:"
+#     read -r bail
+#     if [ "$bail" = 'q' ]
+#       then
+#          exit
+#     fi
+
+# If mirror cannot make connection, it does not create a log file; so we first
+# need to check its direct output, as piped to this script's log
+
+     tail -n 3 $LOG_FILE | grep -E 'Cannot connect' > /dev/null
+     if [ $? = 0 ]
+     then
+        echo "Connect failure in mirror!" | tee -a $LOG_FILE
+        if [ $tries -eq 10 ]
+          then
+             timedout='y'
+             runagain='n'
+             echo "Failed after 10 tries, giving up." | tee -a $LOG_FILE
+             echo "UPDATE appstatus SET status = '$FAILED' \
+               WHERE app_id = 'getV4tesList' AND datestamp = '$rundate';" \
+               | psql -a -d gpmgv  | tee -a $LOG_FILE 2>&1
+          else
+             echo "Sleeping $ZZZ seconds..." | tee -a $LOG_FILE
+             sleep $ZZZ
+        fi
+     else if [ -s $MIR_LOG_FILE ]
+       then
+          #  THIS WAS MODIFIED TO NOT JUST LOOK AT THE ENTIRE LOG FILE, ELSE
+          #  WE END UP RE-TRYING AFTER A SUCCESSFUL TRANSFER IF THERE WAS AN
+	  #  EARLIER FAILURE INDICATED "HIGHER UP" IN THE LOG FILE.
+          #  (FOUND WHEN RUN 1ST TIME AS 'gvoper' AND HAD PERMISSION ISSUES)
+          grep -E '(Fatal|Failed)' ${MIR_LOG_FILE} > /dev/null
+          if [ $? = 0 ]
+          then
+             # check the last line only of the mirror log file to see if we had
+	     # a successful transfer ("Got" in the text) or no files were found
+	     # to transfer ("successful" in the last line) since the prior error
+	     tail -n 1 $MIR_LOG_FILE | grep '(Got|successful)'  > /dev/null
+	     if [$? = 0 ]
+	     then
+                echo "Transfer succeeded after earlier error.  Continuing." \
+	           | tee -a $LOG_FILE
+                runagain='n'
+	     else
+	        echo "Transfer failures in mirror log file!" | tee -a $LOG_FILE
+                if [ $tries -eq 10 ]
+                then
+                   timedout='y'
+                   runagain='n'
+                   echo "Failed after 10 tries, giving up." | tee -a $LOG_FILE
+                else
+                   echo "Sleeping $ZZZ seconds..." | tee -a $LOG_FILE
+                   sleep $ZZZ
+                fi
+	     fi
+          else
+             echo "No error found in mirror log file." | tee -a $LOG_FILE
+             runagain='n'
+          fi
+       else if [ -f $MIR_LOG_FILE ]
+         then
+            # the 'EMPTY FILE' condition probably can't happen, mirror log
+	    # file always exists and has content >IF< mirror gets a connection
+	    echo "Empty mirror log file, no new data to transfer this run."
+            runagain='n'
+         else
+            echo "No log file found!" | tee -a $LOG_FILE
+            runagain='n'
+         fi
+       fi
+     fi
+
+done
+
+# could be more efficient than calling psql in a loop to insert 1 row at a time!
+if [ -s $MIR_LOG_FILE ]
+  then
+    echo "" | tee -a $LOG_FILE
+    # see if any data files were actually downloaded, exit now if none
+    grep ftp_url ${MIR_LOG_FILE} > /dev/null
+    if [ $? != 0 ]
+      then
+        echo "No ftp listings files downloaded; exit." | tee -a $LOG_FILE
+        echo "" | tee -a $LOG_FILE
+        if [ "$timedout" = 'y' ]
+	  then
+            echo "Timed out after $tries failed tries." | tee -a $LOG_FILE
+	         echo "UPDATE appstatus SET status = '$FAILED' \
+             WHERE app_id = 'getV4tesList' AND datestamp = '$rundate';" \
+             | psql -a -d gpmgv  | tee -a $LOG_FILE 2>&1
+	  else
+            echo "UPDATE appstatus SET status = '$MISSING' \
+             WHERE app_id = 'getV4tesList' AND datestamp = '$rundate';" \
+             | psql -a -d gpmgv  | tee -a $LOG_FILE 2>&1
+	fi
+        echo "" | tee -a $LOG_FILE
+        echo "See log file $LOG_FILE for script output."
+        mv -v $MIR_LOG_FILE ${LOG_DIR}/PPSftpListingsBAD.${rundate}.log \
+             | tee -a $LOG_FILE 2>&1
+        exit 1
+      else
+        echo "UPDATE appstatus SET status = '$SUCCESS' \
+          WHERE app_id = 'getV4tesList' AND datestamp = '$rundate';" \
+          | psql -a -d gpmgv  | tee -a $LOG_FILE 2>&1
+    fi
+    echo ""  | tee -a $LOG_FILE
+    echo "Renaming mirror log file to unique/datestamped:"  | tee -a $LOG_FILE
+    mv -v $MIR_LOG_FILE ${LOG_DIR}/PPSftpListings_v4test.${rundate}.log | tee -a $LOG_FILE 2>&1
+fi
+
+#cat $LOG_FILE
+echo ""  | tee -a $LOG_FILE
+echo "SCRIPT getPPSftpListings.sh COMPLETE, EXITING." | tee -a $LOG_FILE
+echo "See log file $LOG_FILE for script output."
+echo "" | tee -a $LOG_FILE
+exit
---
+++ gvoper/merge_11_2_20/notify_vn_status.sh
@@ -0,0 +1,189 @@
+#!/bin/sh
+################################################################################
+#
+#  notify_vn_status.sh     Morris/SAIC/GPM GV     June 2012
+#
+#  DESCRIPTION
+#    Determines the status of the daily operational ingest scripts and puts
+#    together and sends an e-mail message with the summary information.
+#
+#  HISTORY
+#    11/27/13  Morris/GPM GV/SAIC
+#    - Fixed bug in 'if' test for $proccount, had misspelled variable name.
+#    - Moved check for still-running processes up above 'else' for
+#      'if [ -s ${LOG_DIR}/${proc}.${rundate}.log ]'
+#    03/13/14  Morris/GPM GV/SAIC
+#    - Replaced process wgetCTdaily with GPM-era process wgetCTdailies.
+#    03/23/14  Morris/GPM GV/SAIC
+#    - Replaced processes getPRdata, wgetKWAJ_PR_CSI with GPM-era process
+#      get_PPS_CS_data.
+#    08/26/14 - Morris
+#    - Changed LOG_DIR to /data/logs
+#    10/12/14 - Morris
+#    - Changed to only report one instance/format of a downloaded CT file.
+#    - Removed check of Default QC radar files, this script is no longer
+#      pertinent.
+#    03/06/15 - Morris
+#    - Changed to report a count-by-type of all downloaded orbit subset files
+#      rather than a full listing of only downloaded GPM and TRMM files.
+#    - Removed catalogRAWradar from for loop in main script to prevent it from
+#      being reported as missing since it isn't run anymore.
+#    09/11/15 - Morris
+#    - Removed getNAMANLgrids4RainCases from for loop in main script to prevent
+#      it from being reported as missing since it isn't run anymore.
+#    02/01/16 - Morris
+#    - Added metadata processing reporting to script.
+#    06/08/16 - Morris
+#    - Added ground track prediction file processing reporting to script.
+#    06/30/16 - Morris
+#    - Changed "cut -f3 -d '-'" to "cut -f3- -d '-'" to handle "1C-R-CS-subset"
+#      filenames with the extra '-' in the beginning of the name.  Not perfect,
+#      but works.
+#    08/25/16 - Morris
+#    - Added orbit definition file processing reporting to script.
+#
+################################################################################
+#        1         2         3         4         5         6         7         8
+#2345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+MACHINE=`hostname | cut -c1-3`
+case $MACHINE in
+  ds1 ) DATA_DIR=/data/gpmgv ;;
+  ws1 ) DATA_DIR=/data ;;
+    * ) echo "Host unknown, can't set DATA_DIR!"
+        exit 1 ;;
+esac
+
+LOG_DIR=/data/logs
+rundate=`date -u +%y%m%d`
+LOG_FILE=${LOG_DIR}/notify_vn_status.${rundate}.log
+runtime=`date -u`
+VN_STATUS_FILE=/tmp/VN_STATUS_MAIL_MSG.txt
+export VN_STATUS_FILE
+
+umask 0002
+
+function extract_run_info() {
+
+   ls -al ${1} | tee -a $VN_STATUS_FILE
+   echo "" | tee -a $VN_STATUS_FILE
+   s=$1
+   proc_log=${s##*/}
+   proc_id=`echo $proc_log | cut -f1 -d '.'`
+   echo "proc_id: $proc_id"
+
+   case $proc_id in
+     wgetRidgeMosaic ) ngifs=`cat $1 | grep  holding | wc -l`
+                       lastgif=`cat $1 | grep  holding | tail -n1`
+                       echo "# RIDGE Mosaic GIFs downloaded so far:  $ngifs" | tee -a $VN_STATUS_FILE
+                       echo "Latest mosaic: $lastgif" | tee -a $VN_STATUS_FILE ;;
+
+     wgetCTdailies ) echo "CT file downloads and DB conversions:" | tee -a $VN_STATUS_FILE
+#                     cat $1 | grep coincidence_table | grep -vE '(copy|pset)' | tee -a $VN_STATUS_FILE ;;
+                     cat $1 | grep coincidence_table | grep -vE '(Got|copy|pset|unl)' | tee -a $VN_STATUS_FILE ;;
+
+     catalogQCradar ) echo "Final QC UF Radar files cataloged: " | tee -a $VN_STATUS_FILE
+                      cat $1 | grep 1CUF | tee -a $VN_STATUS_FILE ;;
+
+#     getNAMANLgrids4RainCases ) echo "NAM/NAMANL GRIB file download status:" | tee -a $VN_STATUS_FILE
+#                                cat $1 | grep finished | grep -v table | tee -a $VN_STATUS_FILE
+#                                echo "" | tee -a $VN_STATUS_FILE
+#                                cat $1 | grep ERROR | grep -v table | tee -a $VN_STATUS_FILE ;;
+
+     get_PPS_CS_data ) echo "PPS Subset files download status:" | tee -a $VN_STATUS_FILE
+                       for filetypes in `cat $1 | grep Got | grep -v ftp_url |  cut -f3- -d '-' \
+                                        | cut -f 1-4 -d '.' | sort -u`
+                         do
+                           numtype=`cat $1 | grep Got | grep $filetypes | wc -l`
+                           # clean up stuff in pattern after last '-' for e-mail report
+                           echo "${filetypes%-*} :  Got ${numtype} files." | tee -a $VN_STATUS_FILE
+                       done ;;
+
+     RidgeMosaicCTMatch ) echo "CT table loading and RIDGE Mosaic matchups:" | tee -a $VN_STATUS_FILE
+                          cat $1 | grep -i insert | tee -a $VN_STATUS_FILE
+                          cat $1 | grep archivedmosaic | tee -a $VN_STATUS_FILE ;;
+
+     get2ADPRMeta )  echo "Metadata extraction:" | tee -a $VN_STATUS_FILE
+                     line1=`cat $1 | grep file2aDPRsites`   # did we generate control file(s)?
+                     if [ $? = 0 ]
+                       then            # if yes, then report the name(s)
+                         echo $line1 | tee -a $VN_STATUS_FILE
+                         # also find out if anything was produced/loaded by get2ADPRMeta4date.sh
+                         yymmdd=`echo $1 | cut -f2 -d '.'`
+                         metalogdir=${1%/*}
+                         cat $metalogdir/get2ADPRMeta4date.$yymmdd.log | grep Calling \
+                            | tee -a $VN_STATUS_FILE
+                         cat $metalogdir/get2ADPRMeta4date.$yymmdd.log | grep oad \
+                            | grep -v SCRIPT | tee -a $VN_STATUS_FILE
+                       else            # if no Control file, then report missing CT date(s)
+                         cat $1 | grep FATAL | tee -a $VN_STATUS_FILE
+                     fi ;;
+
+     wget_GT7_GPM ) echo "GPM GT prediction download and processing:" | tee -a $VN_STATUS_FILE
+                          cat $1 | grep -E '(GT-7|GPM_1s)' | tee -a $VN_STATUS_FILE ;;
+
+     wget_orbdef_GPM ) echo "GPMCORE ORBDEF download and processing:" | tee -a $VN_STATUS_FILE
+                          line1=`cat $1 | grep Got`
+                          if [ $? = 0 ]
+                            then            # if yes, then report the name(s)
+                              echo $line1 | tee -a $VN_STATUS_FILE
+                            else
+                              echo "No new ORBDEF files downloaded." | tee -a $VN_STATUS_FILE
+                          fi ;;
+
+     * ) echo "Unknown process type: $proc_id" | tee -a $VN_STATUS_FILE ;;
+   esac
+
+   return
+}
+
+#==================================================
+
+# begin main script
+
+#if [ -f $VN_STATUS_FILE ]
+#  then
+#    rm $VN_STATUS_FILE
+#fi
+
+echo "------------------------------------------------------------------------" | tee $VN_STATUS_FILE
+echo " Check gvoper cron-run process statuses on $runtime" | tee -a $VN_STATUS_FILE
+echo "------------------------------------------------------------------------" | tee -a $VN_STATUS_FILE
+echo "" | tee -a $VN_STATUS_FILE
+
+# loop through the log files for the gvoper cron-run parent and child processes
+
+for proc in wgetRidgeMosaic wgetCTdailies RidgeMosaicCTMatch get_PPS_CS_data \
+            catalogQCradar meta_logs/get2ADPRMeta wget_GT7_GPM wget_orbdef_GPM
+  do
+    echo "Looking for ${proc}.${rundate}.log:" | tee -a $VN_STATUS_FILE
+    if [ -s ${LOG_DIR}/${proc}.${rundate}.log ]
+      then
+        proccount=`ps -ef | grep ${proc} | grep -v grep | wc -l`
+        if [ ${proccount} -gt 0 ]
+          then
+            echo "Process for ${proc} is still running:" | tee -a $VN_STATUS_FILE
+            ps -ef | grep ${proc} | grep -v grep | tee -a $VN_STATUS_FILE
+            echo "" | tee -a $VN_STATUS_FILE
+        fi
+        extract_run_info ${LOG_DIR}/${proc}.${rundate}.log
+      else
+        echo "No log file found for ${proc}.${rundate}.log" | tee -a $VN_STATUS_FILE
+    fi
+    echo "" | tee -a $VN_STATUS_FILE
+    echo "---------------------------" | tee -a $VN_STATUS_FILE
+    echo "" | tee -a $VN_STATUS_FILE
+done
+
+if [ -s $VN_STATUS_FILE ]
+  then
+    mail -s 'Processing status on ds1-gpmgv' -c "kbobmorris@hotmail.com,todd.a.berendes@nasa.gov,berendt@uah.edu" \
+         kenneth.r.morris@nasa.gov < $VN_STATUS_FILE
+    cat $VN_STATUS_FILE >> $LOG_FILE
+  else
+    mail -s 'Processing status missing on ds1-gpmgv' kenneth.r.morris@nasa.gov
+fi
+
+echo "See log file $LOG_FILE and status summary $VN_STATUS_FILE"
+
+exit
