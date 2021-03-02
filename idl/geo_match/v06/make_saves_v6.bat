7afbfc20d5f64895104491eb388bf693dcca52d7
tberendes <tberendes@sandman.itsc.uah.edu>
2021-03-02 13:24:29 -0600

    added gmi to make_saves_v6.bat

--- idl/geo_match/v06/make_saves_v6.bat
+++ idl/geo_match/v06/make_saves_v6.bat
@@ -7,3 +7,6 @@ save, /routines, file='dpr2gr_prematch_v6.sav'
 .compile polar2dprgmi_v6.pro
 resolve_all
 save, /routines, file='polar2dprgmi_v6.sav'
+.compile polar2gmi.pro
+resolve_all
+save, /routines, file='polar2gmi.sav'
