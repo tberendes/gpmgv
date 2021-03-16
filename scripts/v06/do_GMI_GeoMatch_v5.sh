ca4d31ef22c924d561b84e6becf0123a0e6e8a8e
tberendes <tberendes@sandman.itsc.uah.edu>
2021-03-16 16:03:37 -0500

    fix

--- scripts/v06/do_GMI_GeoMatch_v5.sh
+++ scripts/v06/do_GMI_GeoMatch_v5.sh
@@ -304,7 +304,7 @@ from eventsatsubrad_vw c JOIN orbit_subset_product o \
   ON c.orbit = o.orbit AND c.subset = o.subset AND c.sat_id = o.sat_id \
  AND o.product_type = '${ALGORITHM2A}' and o.version='${PPS_VERSION}' and o.sat_id='${SAT_ID}' \
    and c.subset IN ('AKradars','CONUS','KWAJ','BrazilRadars','Hawaii') and c.nearest_distance<=${MAX_DIST} \
-   and c.overpass_time at time zone 'UTC' > '${dateStart}' \
+   and c.overpass_time at time zone 'UTC' >= '${dateStart}' \
    and c.overpass_time at time zone 'UTC' < '${dateEnd}' ${site_filter} \
 JOIN rainy100inside100 r on (c.event_num=r.event_num) \
 LEFT OUTER JOIN geo_match_product g on c.event_num=g.event_num \
