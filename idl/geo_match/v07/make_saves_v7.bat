d5dfd7d4023c63e7422634394442e8eddf78ba54
tberendes <tberendes@sandman.itsc.uah.edu>
2021-05-21 13:26:55 -0500

    update

--- idl/geo_match/v07/make_saves_v7.bat
+++ idl/geo_match/v07/make_saves_v7.bat
@@ -4,9 +4,9 @@ save, /routines, file='polar2dpr_hs_fs_v7.sav'
 .compile dpr2gr_prematch_v7.pro
 resolve_all
 save, /routines, file='dpr2gr_prematch_v7.sav'
-;.compile polar2dprgmi_v7.pro
-;resolve_all
-;save, /routines, file='polar2dprgmi_v7.sav'
+.compile polar2dprgmi_v7.pro
+resolve_all
+save, /routines, file='polar2dprgmi_v7.sav'
 .compile polar2gmi.pro
 resolve_all
 save, /routines, file='polar2gmi.sav'
