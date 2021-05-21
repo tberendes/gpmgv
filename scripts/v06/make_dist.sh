61672ee21b0a4ee8d94ae19e7269c97ac2bd0f4d
tberendes <tberendes@sandman.itsc.uah.edu>
2021-05-21 13:35:20 -0500

    Added make_dist.sh scripts to build v6 an v7 distributions of geomatch

---
+++ scripts/v06/make_dist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# this assumes that the git repository for gpmgv is
+# under your home directory and the idl environment
+# has been configured to include the git/gpmgv/idl
+# directory and all of it's subdirectories in the
+# IDL_PATH variable.  Git shoudl be updated by:
+#  cd ~/git/gpmgv
+#  git pul origin master
+mkdir -p v6_geomatch/idl
+mkdir -p v6_geomatch/scripts
+cp ~/git/gpmgv/scripts/v07/* v6_geomatch/scripts
+chmod 755 v6_geomatch/scripts/*
+cp ~/git/gpmgv/idl/geo_match/v06/*.bat v6_geomatch/idl
+cd v6_geomatch/idl
+idl < make_saves_v6.bat
