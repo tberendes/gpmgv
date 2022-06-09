86072b40f4e723a2c1462f3db4e6ced85f3a91fb
tberendes <tberendes@sandman.itsc.uah.edu>
2022-06-09 16:21:27 -0500

    fix

--- scripts/v07/do_DPR2GR_GeoMatch_v7.sh
+++ scripts/v07/do_DPR2GR_GeoMatch_v7.sh
@@ -183,7 +183,8 @@ SAT_ID="GPM"
 export SAT_ID
 SWATH="All"
 
-GEO_MATCH_VERSION=2.1     # current GRtoDPR netCDF matchup file version output by IDL
+#GEO_MATCH_VERSION=2.1     # current GRtoDPR netCDF matchup file version output by IDL
+GEO_MATCH_VERSION=2.2     # current GRtoDPR netCDF matchup file version output by IDL
 # must match version in gen_dpr_geo_match_netcdf_v7.pro
 export GEO_MATCH_VERSION
 
